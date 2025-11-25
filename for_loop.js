b03c92b02b977df345710f941df7328b63930ec1
assapkibasha <assapkibasha@gmail.com>
2025-11-25 12:58:13 +0200

    use of for in

--- for_loop.js
+++ for_loop.js
@@ -15,20 +15,46 @@
 
 //get sum of env and odd nbrs
 
-let numbers = [1,2,3,4,5,6,7,8,9]
-let sumEvn = 0;
+// let numbers = [1,2,3,4,5,6,7,8,9]
+// let sumEvn = 0;
+// let sumOdd = 0;
+
+// for(let i = 0; i < numbers.length; i++){
+//     if(numbers[i] % 2 == 0){
+//         sumEvn += numbers[i];
+//     }else{
+//         sumOdd +=numbers[i]  
+//     }
+// }
+
+
+// // let difference = sumEvn - sumOdd;
+// console.log(`sum of even is :${sumEvn}`)
+// console.log(`the sum of odd : ${sumOdd}`)
+// console.log(`the difference is (sum of Even) = ${sumEvn} - (sum of Odd) = ${sumOdd} = ${sumEvn - sumOdd}`)
+
+
+// let fruits = ["Kellia","Coco", 'niha','hadidja']
+
+// for(i in fruits){
+//     console.log(fruits[i])
+// }
+
+let obj = {
+    'num1':1,
+    'num2':2,
+    'num3':3,
+    'num4':4,
+}
+let sumEven = 0;
 let sumOdd = 0;
 
-for(let i = 0; i < numbers.length; i++){
-    if(numbers[i] % 2 == 0){
-        sumEvn += numbers[i];
+for(i in obj){
+    if(obj[i] %2 == 0){
+        sumEven += obj[i]
     }else{
-        sumOdd +=numbers[i]  
+        sumOdd += obj[i]
     }
 }
 
-
-// let difference = sumEvn - sumOdd;
-console.log(`sum of even is :${sumEvn}`)
-console.log(`the sum of odd : ${sumOdd}`)
-console.log(`the difference is (sum of Even) = ${sumEvn} - (sum of Odd) = ${sumOdd} = ${sumEvn - sumOdd}`)
+console.log(`the sum of Even is : ${sumEven} and sum of odd is : ${sumOdd} while difference is : ${sumEven - sumOdd}`)
