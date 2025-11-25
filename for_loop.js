340cf2c8691c33fa4ce4dc457627bca53fb85e3d
assapkibasha <assapkibasha@gmail.com>
2025-11-25 09:45:00 +0200

    for_loop

---
+++ for_loop.js
@@ -0,0 +1,16 @@
+//for(initial_value; condition; Modifier){ //block of code}
+//used to add break end = ''
+
+// for(let i = 1; i<= 100; i++){
+//     console.log(i, end = '');
+// }
+
+//loop through array
+
+let arr = [20,30,40,50,60,70];
+
+for(let i = 0; i<= (arr.length) - 1; i++){
+    console.log(arr[i], end = '');
+}
+
+//add all elements of array
