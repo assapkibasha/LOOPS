6ba77c84af07737c6682a01af055685dc795d545
assapkibasha <assapkibasha@gmail.com>
2025-11-25 10:44:33 +0200

    number pyramid

--- for_loop.js
+++ for_loop.js
@@ -7,10 +7,28 @@
 
 //loop through array
 
-let arr = [20,30,40,50,60,70];
+// let arr = [20,30,40,50,60,70];
 
-for(let i = 0; i<= (arr.length) - 1; i++){
-    console.log(arr[i], end = '');
+// for(let i = 0; i<= (arr.length) -1; i++){
+//     console.log(arr[i], end = '');
+// }
+
+//get sum of env and odd nbrs
+
+let numbers = [1,2,3,4,5,6,7,8,9]
+let sumEvn = 0;
+let sumOdd = 0;
+
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] % 2 == 0){
+        sumEvn += numbers[i];
+    }else{
+        sumOdd +=numbers[i]  
+    }
 }
 
-//add all elements of array
+
+// let difference = sumEvn - sumOdd;
+console.log(`sum of even is :${sumEvn}`)
+console.log(`the sum of odd : ${sumOdd}`)
+console.log(`the difference is (sum of Even) = ${sumEvn} - (sum of Odd) = ${sumOdd} = ${sumEvn - sumOdd}`)
---
+++ star.js
@@ -0,0 +1,11 @@
+//display pyramid of numbers
+
+for(let i =1; i<=4; i++){
+    let row = ' '
+    for(let j =1; j<=i; j++){
+        row+= '*';
+    }
+    console.log(row)
+
+}
+
